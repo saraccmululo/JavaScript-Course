84a5eb759d7d6e66da4c99b409b8707cce39e18b
Sara Costa Cabral Mululo <saramululo@gmail.com>
2024-11-13 15:26:11 -0600

    Create ambiente.js

---
+++ JavaScript Prof Guanabara Course/Aula13/ambiente.js
@@ -0,0 +1,18 @@
+var i=1
+do{
+    console.log(`Passo ${i}`)
+    i++ //i=i+1
+} while(i<=6)
+
+/*var i=1
+while(i<=6){
+    console.log(`Passo ${i}`)
+    i++ //i=i+1
+}*/
+
+/*console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')*/
